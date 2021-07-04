512e028fad43ab4273f01fdd9520725f5f5a57ec
Kaushal Diswar <86912351+StrangeKausu@users.noreply.github.com>
2021-07-04 20:04:26 +0530

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+int main() {
 int a;
     cout<<"Enter your No. ";
     cin>>a;
     for (int i=1;i<11;i++){
       cout<<a<<" x "<<i<<" = "<<i*a<<"\n";
     }
+    return 0;
+}
